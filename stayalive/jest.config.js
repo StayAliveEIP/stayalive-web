c2f64b2f364af6b1ecec0cb35e1edd4704c5c346
Matthieu33197 <matthieu.queru@epitech.eu>
2023-06-05 13:43:19 +0200

    [FIX] jest test

--- stayalive/jest.config.js
+++ stayalive/jest.config.js
@@ -2,11 +2,9 @@ module.exports = {
     preset: 'ts-jest',
     transform: {
         "^.+\\.(t|j)sx?$": "ts-jest",
+        '^.+\\.css$': 'jest-css-modules-transform'
     },
     testEnvironment: 'jest-environment-jsdom',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    moduleNameMapper: {
-      '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-    },
   };
   
