2e5a145495a29dece4ceb6c2982774765fb23c79
Matthieu33197 <matthieu.queru@epitech.eu>
2023-06-05 14:06:14 +0200

    [FIX] jest test

--- stayalive/jest.config.js
+++ stayalive/jest.config.js
@@ -1,10 +1,21 @@
-module.exports = {
-    preset: 'ts-jest',
-    transform: {
-        "^.+\\.(t|j)sx?$": "ts-jest",
-        '^.+\\.css$': 'jest-css-modules-transform'
-    },
-    testEnvironment: 'jest-environment-jsdom',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  };
-  
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  moduleNameMapper: {
+    "^@/components/(.*)$": "<rootDir>/components/$1",
+    "^@/connexion/(.*)$": "<rootDir>/connexion/$1",
+    "^@/inscription/(.*)$": "<rootDir>/inscription/$1",
+  },
+  testEnvironment: "jest-environment-jsdom",
+};
+
+module.exports = async () => ({
+  ...(await createJestConfig(customJestConfig)()),
+  transformIgnorePatterns: [
+    '/node_modules/(?!(firebase|@firebase)/)',
+  ],
+});
