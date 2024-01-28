989490670d7903e0849fee10674309a67f3f025c
Matthieu33197 <matthieu.queru@epitech.eu>
2024-01-28 21:45:03 +0900

    [add] final connexion

--- stayalive/src/app/Profil/components/RescueCounter.tsx
+++ stayalive/src/app/Profil/components/RescueCounter.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import styles from './Rescue.module.css'
+import React from 'react';
+import styles from './Rescue.module.css';
 
-function RescueCounter({ count }) {
-    return (
-      <div className={styles.rescueSection}>
-        <h2 className={styles.rescueTitle}>Nombre de Sauvetage</h2>
-        <div className={styles.circle}>
-          {count}
-        </div>
+// Définissez une interface pour les props
+interface RescueCounterProps {
+  count: number; // Spécifiez que 'count' est un nombre
+}
+
+function RescueCounter({ count }: RescueCounterProps) {
+  return (
+    <div className={styles.rescueSection}>
+      <h2 className={styles.rescueTitle}>Nombre de Sauvetage</h2>
+      <div className={styles.circle}>
+        {count}
       </div>
-    )
-  }
-  
-  export default RescueCounter
+    </div>
+  );
+}
+
+export default RescueCounter;
