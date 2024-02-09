6cb7da13d7164be22d4949e8808952553123df60
Matthieu33197 <matthieu.queru@epitech.eu>
2024-02-09 22:14:36 +0900

    [FIX] navbar image

--- stayalive/src/app/components/NavBarD.tsx
+++ stayalive/src/app/components/NavBarD.tsx
@@ -24,7 +24,7 @@ const NavbarD: React.FC<{ isLoginPage: boolean }> = ({ isLoginPage }) => {
     <nav className={styles.nav}>
       <div className={`${styles.logo}`}>
         <Link href="/" legacyBehavior>
-          <Image src="https://media.discordapp.net/attachments/1019600215545749587/1019610952116748298/Screenshot_2022-09-14_at_15.48.00.png?width=1284&height=1294" alt="Description de l'image" width="40" height="31" />
+          <Image src="https://media.discordapp.net/attachments/1130401857890697285/1190320898281066607/logo_Background_Removed.png?ex=65cf8439&is=65bd0f39&hm=b9498b5ab0627f56c8394b65b442358eb82c65d735b03c95f2c06290f3052de2&=&format=webp&quality=lossless&width=936&height=910" alt="Description de l'image" width="40" height="31" />
         </Link>
         <span className={styles.logoText}>StayAlive</span>
         <div className={styles.buttons}>
