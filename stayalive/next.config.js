68254da62c3c434416a65561aa6b3102099e3a7d
Matthieu33197 <matthieu.queru@epitech.eu>
2024-02-28 16:02:40 +0900

    [fix] picture

--- stayalive/next.config.js
+++ stayalive/next.config.js
@@ -1,20 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = {
-    images: {
-      domains: ['media.discordapp.net', 'www.freepnglogos.com'],
-    },
-    async rewrites() {
-      return [
-        {
-          source: '/auth/register',
-          destination: 'http://api.stayalive.fr:3000/auth/register',
-        },
-        {
-          source: '/auth/login',
-          destination: 'http://api.stayalive.fr:3000/auth/login',
-        },
-      ];
-    },
-  };
+const nextConfig = {
+  reactStrictMode: true,
+  i18n,
+  sassOptions: {
+      includePaths: [path.join(__dirname, 'src/styles')],
+      prependData: `@import "variables.scss";`,
+  },
+  images: {
+      remotePatterns: [
+          {
+              protocol: 'https',
+              hostname: '**',
+              port: '',
+              pathname: '**',
+          },
+      ],
+  },
+}
