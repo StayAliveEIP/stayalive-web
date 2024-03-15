921a833e6d87ad9fa09d823a0039c4e8d413af6d
Matthieu33197 <matthieu.queru@epitech.eu>
2024-03-15 20:35:01 +0900

    [FIX] error title on landing page

--- stayalive/src/app/page.tsx
+++ stayalive/src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
         className="z-50 flex flex-col justify-center items-center"
       >
         <motion.p className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
-          Tous les héros ne portent <br /> tous une cape
+          Tous les héros ne portent pas <br /> tous une cape
         </motion.p>
         <Link href="/inscription/sauveteur">
         <button className="px-4 py-2 backdrop-blur-sm border bg-[#c42f30]/10 border-[#c42f30]/20 text-white mx-auto text-center rounded-full relative mt-4 hover:bg-[#c42f30]/50">
